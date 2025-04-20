6b3e0811a82c82344c568bd47d43c51b6c233aca
pguzmanUTN <pguzman@frba.utn.edu.ar>
2025-04-20 19:52:36 -0300

    20/4/2025 19:52

--- Semanal#2/Semanal#2.py
+++ Semanal#2/Semanal#2.py
@@ -15,28 +15,18 @@ from scipy.signal import TransferFunction
 import scipy.signal as signal
 import matplotlib.pyplot as plt
 import numpy as np
-
+#import math
 # Librería de TC2, esta la vas a usar mucho
 from pytc2.sistemas_lineales import pzmap
-w0=2*3.1415*10000
-wz=R5=1000
-Cn=10e-9*w0*wz
-Cn2=10e-9*w0*wz
-Rn4=Rn1=9000/wz
-Rn=31830/wz
-Rn3=2533/wz
 
-R4=R1=Rn1*wz
-R3=Rn3*wz
-R=Rn*wz
-C=Cn/(wz*w0)
-C2=Cn2/(wz*w0)
-
+w0=1
+Q=1
+K=-1
 
 
 # Definición de la función de transferencia
-numerador = [(1+Rn4)*(1/(Rn*Cn)) , 0] 
-denominador = [1, 1/(Rn*Cn), Rn4/(Rn1*Rn3*Cn*Cn2)]  
+numerador = [K*w0/Q] 
+denominador = [1, w0/Q, w0**2]  
 
 # Crear la función de transferencia
 my_tf = TransferFunction( numerador, denominador)
@@ -61,44 +51,6 @@ plt.grid()
 plt.subplot(2, 1, 2)
 plt.semilogx(w, fase)
 plt.xlabel('Frecuencia (rad/s)')
-plt.ylabel('Fase (grados)')
-plt.grid()
-
-
-
-plt.show()
-
-plt.close('all')
-
-pzmap(my_tf, fig_id=2) #S plane pole/zero plot
-
-# Definición de la función de transferencia 
-numerador = [(1+R4/R5)*(1/(R*C)) , 0]  
-denominador = [1, 1/(R*C), R4/(R1*R3*R5*C*C2)]  
-
-# Crear la función de transferencia
-my_tf = TransferFunction( numerador, denominador)
-
-# Define el rango de frecuencias para la simulación
-frecuencias = np.logspace(0, 6, 500)  # Rango de frecuencias
-
-# Calcular la respuesta en frecuencia
-w, mag, fase = signal.bode(my_tf, frecuencias)
-
-# Graficar la magnitud
-plt.figure(figsize=(10, 6))
-
-# Magnitud (en dB)
-plt.subplot(2, 1, 1)
-plt.semilogx(w/(2*3.1416), mag)
-plt.title('Respuesta en Magnitud y Fase de la Transferencia')
-plt.ylabel('Modulo (Amplitud)')
-plt.grid()
-
-# Fase
-plt.subplot(2, 1, 2)
-plt.semilogx(w/(2*3.1416), fase)
-plt.xlabel('Frecuencia (Hz)')
 plt.ylabel('Fase (grados)')
 plt.grid()
 
